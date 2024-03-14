33e992d16cbd94c4959502faaaaaab023dc79cd1
farleybsd <farley.t.i@hotmail.com>
2024-03-14 20:43:47 -0300

    Ordenacao

---
+++ Algoritmo.Csharp.Com/BubbleSorting/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/*
+ *  Algoritmo de Ordernacao de Bolhas
+ *  Algoritmo de ordenacao de bolha (Bubble Sorting)
+ *  Se arrays [j] > arrays [j + 1] forem trocados 
+ *  Os elementos restante se repetem ate a ordenacao for concluida 
+ */
+
+
+/*
+ *  Desafio : Realizar uma Ordenacao do Menor para o Maior
+ *  Input: 60,50,95,80,70
+ *  Resultado: 50,60,70,80,85,90
+ */
+
+using BubbleSorting;
+
+var ordenacao = new TestBubbleSort();
+ordenacao.Main();
---
+++ Algoritmo.Csharp.Com/BubbleSorting/TestBubbleSort.cs
@@ -0,0 +1,39 @@
+﻿
+namespace BubbleSorting
+{
+    public class TestBubbleSort
+    {
+        public void Main()
+        {
+            int[] scores = { 90, 70, 50, 80,60,85 };
+            sort(scores);
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.Write(scores[i] + ",");
+            }
+        }
+
+        public void sort(int[] arrays)
+        {
+            for (int i = 0; i < arrays.Length - 1; i++)
+            {
+                bool trocar = false;
+
+                for (int j = 0; j  < arrays.Length - 1; j++)
+                {
+                    if (arrays[j] > arrays[j + 1]) // Faz a Troca
+                    {
+                        int flag = arrays[j];
+                        arrays[j] = arrays[j + 1];
+                        arrays[j + 1] = flag;
+                        trocar = true;
+                        if (!trocar) // Nao faz a troca e encerra a ordenacao
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
